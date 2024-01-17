f894752bb3af7371d04cccc9074051e62c03b723
ANonABento <kevin.jiang53@gmail.com>
2024-01-17 11:31:12 -0500

    init

---
+++ TheCloset/src/main/java/ICSProject/TheCloset/App.java
@@ -0,0 +1,13 @@
+package ICSProject.TheCloset;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
