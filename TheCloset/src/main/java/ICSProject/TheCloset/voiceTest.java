0222e38a5f42ef7a8b7edda00ce0264c237b3907
ANonABento <kevin.jiang53@gmail.com>
2024-01-22 05:16:31 -0500

    testing mbrola voices and got it to work

--- TheCloset/src/main/java/ICSProject/TheCloset/voiceTest.java
+++ TheCloset/src/main/java/ICSProject/TheCloset/voiceTest.java
@@ -8,14 +8,18 @@ public class voiceTest {
 	private static Voice voice;
 	
 	public voiceTest() {
-		System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
-		voice = VoiceManager.getInstance().getVoice("kevin16");
+		// Set the path to the MBROLA base directory
+        System.setProperty("mbrola.base", "C:\\Users\\kevin\\git\\TheCloset\\TheCloset\\mbrola");
+        // Set the FreeTTS voices to use the MbrolaVoiceDirectory
+        System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory,de.dfki.lt.freetts.en.us.MbrolaVoiceDirectory");
+        
+		voice = VoiceManager.getInstance().getVoice("mbrola_us1");
 		
 		if (voice != null) {
 	        voice.allocate();// Allocating Voice
 	        try {
-	            voice.setRate(190);// Setting the rate of the voice
-	            voice.setPitch(150);// Setting the Pitch of the voice
+	            voice.setRate(170);// Setting the rate of the voice
+	            voice.setPitch(210);// Setting the Pitch of the voice
 	            voice.setVolume(3);// Setting the volume of the voice
 
 	        } catch (Exception e1) {
---
+++ TheCloset/src/main/java/ICSProject/TheCloset/voicetest2.java
@@ -0,0 +1,27 @@
+package ICSProject.TheCloset;
+
+import com.sun.speech.freetts.Voice;
+import com.sun.speech.freetts.VoiceManager;
+
+public class voicetest2 {
+    private static Voice[] voices;
+
+    public static void main(String[] args) throws Exception {
+        // Set the path to the MBROLA base directory
+        System.setProperty("mbrola.base", "C:\\Users\\kevin\\git\\TheCloset\\TheCloset\\mbrola");
+
+        // Set the FreeTTS voices to use the MbrolaVoiceDirectory
+        System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory,de.dfki.lt.freetts.en.us.MbrolaVoiceDirectory");
+
+        VoiceManager vm;
+        // VoiceManager uses the singleton approach to creating and providing an instance
+        vm = VoiceManager.getInstance();
+        // Get all the voices which the VoiceManager knows about
+        voices = vm.getVoices();
+
+        for (Voice voice : voices) {
+            // Find out what voices are available.
+            System.out.println(voice.getName() + " - " + voice.getDescription());
+        }
+    }
+}
